b51926c13b064a42c99779bdc43d4ef4b1dc5704
jorgemunozssc <93778130+jorgemunozssc@users.noreply.github.com>
2022-09-07 06:04:22 -0600

    Create select_sms.js

---
+++ select_sms.js
@@ -0,0 +1,21 @@
+if (BIS.urlIsProductPage() === true) {
+  BISPopover.ready.then(function() {
+
+    // Exit script if product has no sold out variants.
+    if (BIS.popup.variants.length < 1) {
+      return;
+    }
+
+    var smsClick = function() {
+      var iframe = document.getElementById('BIS_frame');
+      var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
+      var button = innerDoc.querySelector('#customer-contact-container .btn-group:nth-child(2) button')
+      if (!button.classList.contains('active')) button.click()
+    }
+
+    document.querySelector('#BIS_trigger').addEventListener('click', function() {
+      setTimeout(smsClick, 50) 
+    })
+    
+  })
+}
