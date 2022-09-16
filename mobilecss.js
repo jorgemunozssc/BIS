850f2e2b09f4499f1b97ee86ade8b0e88c164a5c
jorgemunozssc <93778130+jorgemunozssc@users.noreply.github.com>
2022-09-16 12:36:34 -0600

    Create mobilecss.js

---
+++ mobilecss.js
@@ -0,0 +1,5 @@
+ var isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+         
+           if (isMobile == true) {
+            button.style.cssText = 'width: 100%; background-color: #D9121F; font-size: 1em;';
+            console.log("mobile on"); }       
